2f4e1501b8877928dc9acdd8685a98de7a7f85a5
alexbarzaga <86619022+alexbarzaga@users.noreply.github.com>
2022-11-18 22:40:58 -0500

    Create CollissionDetectionSystem.py

---
+++ CollissionDetectionSystem.py
@@ -0,0 +1,20 @@
+class CollissionDetectionSystem:
+    def __init__(self) -> None:
+        print("Sensor constructor")
+
+    def __init__(self) -> None:
+        print("Bumper constructor")
+
+    def movementPermission(self, judgement): 
+        print(f"Robot {judgement} move!")
+
+    def reroute(self): 
+        print(f"Reroute neccessary!")
+
+    def obstacleDetected(self): 
+        print(f"An obstacle has been detected!")
+
+    def queuePositionOpen(self): 
+        print(f"The queue has an open position!")
+
+    
