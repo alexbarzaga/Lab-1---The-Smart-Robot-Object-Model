6056a0d15cfa4ee3d2d0da2ef8a3d33647e691db
alexbarzaga <86619022+alexbarzaga@users.noreply.github.com>
2022-11-18 22:32:19 -0500

    PowerSystem class created!

---
+++ PowerSystem.py
@@ -0,0 +1,17 @@
+class PowerSystem:
+    def __init__(self) -> None:
+        print("Battery constructor")
+
+    def __init__(self) -> None:
+        print("Battery percentage constructor")
+
+    def __init__(self) -> None:
+        print("Timer constructor")
+
+    def charging(self): 
+        print(f"Battery charging!")
+
+    def powerMotor(self): 
+        print(f"Motor is alive!")
+
+   
