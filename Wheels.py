f0a72b830754fbdc095a9cbfabd1cc4ccd0986c9
alexbarzaga <86619022+alexbarzaga@users.noreply.github.com>
2022-11-18 22:36:10 -0500

    Create Wheels.py

---
+++ Wheels.py
@@ -0,0 +1,14 @@
+class Wheels:
+    def __init__(self) -> None:
+        print("Material constructor")
+
+    def __init__(self) -> None:
+        print("Size constructor")
+
+    def __init__(self) -> None:
+        print("Position constructor")
+
+    def moveForward(self): 
+        print(f"Wheels are spinning!")
+
+
